80efa1d9aae56aad6e7e7cde0d92dfa1c36cb3fa
cesarsoriano <CYSORIANO@sky.com.mx>
2024-08-26 12:52:42 -0600

    > Show map on screen

--- app/src/main/java/com/yaelsoriano/uberclondriverkotlin/activities/MapActivity.kt
+++ app/src/main/java/com/yaelsoriano/uberclondriverkotlin/activities/MapActivity.kt
@@ -3,15 +3,26 @@ package com.yaelsoriano.uberclondriverkotlin.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.yaelsoriano.uberclondriverkotlin.R
 import com.yaelsoriano.uberclondriverkotlin.databinding.ActivityMapBinding
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapBinding
+    private var googleMap: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
     }
 }
